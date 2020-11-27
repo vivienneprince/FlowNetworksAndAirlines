d4598ab639962cff05fbf52aea4fe01243acce71
vivienneprince <vivie.prince@gmail.com>
2020-11-27 17:23:13 -0500

    Update 01_Data_Munging.py

--- 01_Data_Munging.py
+++ 01_Data_Munging.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from pathlib import Path
-import requests
-import lxml.html as lh
+
 
 
 # import given txt data
@@ -14,5 +13,12 @@ routes = pd.read_csv(datpath / 'routes.dat.txt', header=None)  # import routes d
 routes.columns = list(routes_info[:]['routes_columnid'])
 
 planes = pd.read_csv(datpath / 'planes.dat.txt', header=None)  # import planes data
-planes.columns = ['plane_name','equipment_id','plane_abbrv']
-print(planes)
+planes.columns = ['plane_name', 'equipment_id', 'plane_abbrev']
+
+URL = 'https://blog.thetravelinsider.info/airplane-types'  # import planes info from site
+planes_info = pd.read_html(URL, header=2)[0]
+
+
+# clean up planes info and merge it with planes
+
+print(planes_info)

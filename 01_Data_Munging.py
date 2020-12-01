9bf34566605a1eb3e65eaa8c179de20bdfe629e7
vivienneprince <wei-chun.tien13@ncf.edu>
2020-11-30 19:25:21 -0500

    Update 01_Data_Munging.py

--- 01_Data_Munging.py
+++ 01_Data_Munging.py
@@ -1,33 +1,9 @@
-import pandas as pd
 from pathlib import Path
+import pandas as pd
+from selenium import webdriver
 
-###################################################################
-###                     Initial Data                            ###
-###################################################################
 
 datpath = Path('data')
-
-routes_info = pd.read_csv(datpath / 'routes_info.csv', header=None)  # routes info was c/p from assignment instructions
-routes_info.columns = ['routes_columnid', 'description']
-
-routes = pd.read_csv(datpath / 'routes.dat.txt', header=None)  # import routes data
-routes.columns = list(routes_info[:]['routes_columnid'])
-
-planes = pd.read_csv(datpath / 'planes.dat.txt', header=None)  # import planes data
-planes.columns = ['plane_name', 'equipment_id', 'plane_abbrev']
-
-# URL = 'https://blog.thetravelinsider.info/airplane-types'  # import planes info from site
-# planes_info = pd.read_html(URL, header=2)[0]
-
-
-###################################################################
-###                     Capacity Data                           ###
-###################################################################
-
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from selenium import webdriver
 
 # Routes dataset
 
